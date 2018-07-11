69ee7fbd3370bab3d8b22f4e2e6340b98c3b8f12
Anton Kiselev <Anton_Kiselev3@epam.com>
2018-07-11 18:14:31 +0300

    -| revert unnecessary changes

--- FBSnapshotTestCase/FBSnapshotTestController.m
+++ FBSnapshotTestCase/FBSnapshotTestController.m
@@ -1,10 +1,9 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
+ *  Copyright (c) 2017-2018, Uber Technologies, Inc.
+ *  Copyright (c) 2015-2018, Facebook, Inc.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
  *
  */
 
@@ -47,7 +46,8 @@ typedef NS_ENUM(NSUInteger, FBTestSnapshotFileNameType) {
   if (self = [super init]) {
     _testName = [testName copy];
     _deviceAgnostic = NO;
-    
+    _agnosticOptions = FBSnapshotTestCaseAgnosticOptionNone;
+
     _fileManager = [[NSFileManager alloc] init];
   }
   return self;
